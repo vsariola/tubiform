7b9c7903774cfec385d9de6e447efa2f09255694
Veikko Sariola <5684185+vsariola@users.noreply.github.com>
2022-01-01 02:06:29 +0200

    Add support for PC Speaker

---
+++ build.bat
@@ -0,0 +1,2 @@
+nasm venx.asm -fbin -o venx.com
+nasm venx.asm -DSPEAKER -fbin -o venxspkr.com
--- venx.asm
+++ venx.asm
@@ -1,12 +1,25 @@
 org 100h
 
 ; at startup, ax = 0x0000, cx = 0x00FF, si = 0x0100, sp = 0xFFFE and most flags are zero
+%ifdef SPEAKER
+    mov     ax, 13h
+    int     10h
+    mov     ax, 3508h			        ; int 21h: ah=35h get interrupt handler, al=1Ch which interrupt
+    int     21h					        ; returns the handler in es:bx
+    push    es
+    push    bx
+    xchg    ax, cx                      ; PIT counter divisor, al = 255. Irq init based on superogue's code.
+    mov     al, 90h
+    out     43h, al
+    add     al, 64-90h                  ; PC speaker needs to run at much higher frequency
+%else
     mov     ax, 351ch			        ; int 21h: ah=35h get interrupt handler, al=1Ch which interrupt
     int     21h					        ; returns the handler in es:bx
     push    es
     push    bx
-    scaleconst equ $
     xchg    ax, cx                      ; PIT counter divisor, al = 255. Irq init based on superogue's code.
+%endif
+    scaleconst equ $-1
     mov     dx, irq                     ; new handler address
     mov     bl, 0x13
 envs:
@@ -27,12 +40,21 @@ main:                                   ; basic tunnel effect, based on Hellmood
     fimul	dword [byte si+scaleconst]  ; fpu: const*cos(theta) theta, the constant is what ever the lines there assemble to
     .rscale equ $-1
     fidiv	word [bx-1]	                ; fpu: const*sin(theta)/x/256=1/r theta
+%ifdef SPEAKER
+    fisub	word [time]                 ; fpu: 1/r+offset theta
+%else
     fisub	word [byte si+time]         ; fpu: 1/r+offset theta
+%endif
     fistp	dword [bx]                ; store r+offset to where dx is, cx&dx affected after popa, fpu: theta
     fnop                                ; this fnop will mutated to something more interesting eventually
     .effect2 equ $-1
+%ifdef SPEAKER
+    fimul	word [time+3]	            ; fpu: t*theta (+2 is initially wrong, but will be replaced with time+0 i.e. correct)
+    .thetascale equ $-2
+%else
     fimul	word [byte si+time+3]	    ; fpu: t*theta (+2 is initially wrong, but will be replaced with time+0 i.e. correct)
     .thetascale equ $-1
+%endif
     fistp	dword [bx+2]                ; store r+offset to where cx is, cx&ax affected after popa,
     popa				                ; pop all registers from stack
     mov     al, byte [byte si+envs+2]   ; we add together the last two envelopes
@@ -60,13 +82,28 @@ main:                                   ; basic tunnel effect, based on Hellmood
     pop     ds
     mov     bl, 3
 setirq:
+%ifdef SPEAKER
+    out     40h, al                     ; write PIT counter divisor low byte
+    mov     al, 0
+    out     40h, al
+    salc
+    out     61h, al
+    mov     ax, 2508h                   ; al = which PIT timer interrupt tos set: 08 or 1c. 1c gets called after 08
+    int     21h                         ; ah = 25h => set interrupt handler, al = which interrupt. Tomcat: "standard INT08 rutine call INT1C after its own business"
+    cmp     bl, 3
+    jne     .cont
+    movzx   ax, bl
+    int     10h
+.cont:
+%else
     out     40h, al                     ; write PIT counter divisor low byte
     salc                                ; set AL = 0 (because carry is zero)
     out     40h, al	                    ; write PIT counter divisor high byte (freq = 1,19318181818 MHz / divisor)
-    mov     al, bl
+    xchg    al, bl
     int     10h
     mov     ax, 251ch                   ; al = which PIT timer interrupt tos set: 08 or 1c. 1c gets called after 08
     int     21h                         ; ah = 25h => set interrupt handler, al = which interrupt. Tomcat: "standard INT08 rutine call INT1C after its own business"
+%endif
     ret
 
 
@@ -92,13 +129,31 @@ patterns:
 
 irq:
     pusha
+%ifdef SPEAKER
+    mov     ax, 4
+    .sample equ $-2
+    mov     cx, 346         ; mastering
+    mul     cx
+    shr     ax, 10
+    jz      .skipout
+    out     42h, al
+.skipout:
+%endif
+    push    ds
+    push    cs
+    pop     ds
+%ifdef SPEAKER
+    dec     byte [counter]
+    jnz     .skipirq
+    mov     byte [counter], 4
+%endif
     xor     di, di
     mov     cx, 3                           ; cx is the channel loop counter, we have three channels
     mov     si, time
     mov     bx, patterns-1
 .loop:
     mov     bp, cx                          ; TomCat: "[in IRQ on DOS] SS could be different than CS so indexing with BP could be a pain!"
-    mov     al, byte [byte orderlist-1+si-time+bp]
+    mov     al, byte [cs:byte orderlist-1+si-time+bp]
     .pattern equ $ - 1
     aam     16
     jz      .skipchannel                    ; if pattern is zero, skip this channel
@@ -113,13 +168,15 @@ irq:
     imul    ax, word [si]                   ; t*freq
     sahf                                    ; square wave
     jns      .skipchannel                   ; you can test different flags here to shift song up/down octaves
-    mov     byte [envs-1+bp+si-time], dl ; save the envelope for visuals
+    mov     byte [cs:envs-1+bp+si-time], dl ; save the envelope for visuals
     add     di, dx                          ; add channel to sample total
 .skipchannel:
     loop    .loop
     xchg    ax, di
+%ifndef SPEAKER
     mov     dx, 0378h                       ; LPT1 parallel port address
     out     dx, al		                    ; write 8 Bit sample data
+%endif
     dec     word [si]                       ; the time runs backwards to have decaying envelopes
     js      .skipnextpattern
     mov     ax, word [orderlist+3]
@@ -130,5 +187,19 @@ irq:
     add     byte [.pattern+si-time],5       ; modify the movzx instruction
     add     word [.script+si-time],5
 .skipnextpattern:
+%ifdef SPEAKER
+    mov     byte [irq.sample], al
+%endif
+.skipirq:
+    pop     ds
+%ifdef SPEAKER
+    mov     al, 20h
+    out     20h, al                         ; end of line for the interrupt
+%endif
     popa
     iret
+
+%ifdef SPEAKER
+counter:
+    db 4
+%endif
