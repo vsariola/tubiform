37a224f4766c659fe80913ff44db39b7c37d7abe
Veikko Sariola <5684185+vsariola@users.noreply.github.com>
2022-01-14 10:19:06 +0200

    Convert tabs to spaces

--- spkrtubi.asm
+++ spkrtubi.asm
@@ -1,8 +1,8 @@
 org 100h
 
 ; at startup, we assume ax = 0x0000, cx = 0x00FF, si = 0x0100, sp = 0xFFFE and most flags zero
-    mov     ax, 3508h			        ; int 21h: ah=35h get interrupt handler, al=1Ch which interrupt
-    int     21h					        ; returns the handler in es:bx
+    mov     ax, 3508h                   ; int 21h: ah=35h get interrupt handler, al=1Ch which interrupt
+    int     21h                         ; returns the handler in es:bx
     push    es                          ; save the current handler to be able to restore it
     push    bx
     mov     ax, 64                      ; PIT counter divisor, al = 64. IRQ init based on superogue's and TomCat's code.
@@ -13,31 +13,31 @@ envs:                                   ; envs contains the envelopes of the cha
     call    setirq                      ; into known values (in particular, to have chn 3 initialize as 0).
     xchg    ax, cx
     out     61h, al                     ; enable PC speaker
-    push 	0xa000 - 10-20*3            ; set es to video segment, shifting 3.5 lines (the top three lines had some visual glitch).
-    pop 	es
+    push    0xa000 - 10-20*3            ; set es to video segment, shifting 3.5 lines (the top three lines had some visual glitch).
+    pop     es
 main:                                   ; basic tunnel effect, based on HellMood's original from http://www.sizecoding.org/wiki/Floating-point_Opcodes#The_.22Tunnel.22
-    sub		dh, 0x68                    ; dh = y, shift it to center the coordinates
-    pusha				                ; push all registers to stack 0xFFF8: ax, 0xFFF6: cx, 0xFFF4: dx, etc. bx, sp, bp, si, di
+    sub     dh, 0x68                    ; dh = y, shift it to center the coordinates
+    pusha                               ; push all registers to stack 0xFFF8: ax, 0xFFF6: cx, 0xFFF4: dx, etc. bx, sp, bp, si, di
     mov     bx,-12                      ; 0xFFF4, where the dx is
-    fild 	word [bx-1]	                ; fpu: x*256
-    fild 	word [bx]                   ; fpu: y*256(+x) x*256
-    fpatan				                ; fpu: theta
-    fst 	st1			                ; fpu: theta theta
-    fprem				                ; this instruction will be mutated with fsin so for proper tunnel, fpu: sin(theta) theta
+    fild    word [bx-1]                 ; fpu: x*256
+    fild    word [bx]                   ; fpu: y*256(+x) x*256
+    fpatan                              ; fpu: theta
+    fst     st1                         ; fpu: theta theta
+    fprem                               ; this instruction will be mutated with fsin so for proper tunnel, fpu: sin(theta) theta
     .effect equ $-1                     ; 0xF3, 0xF4, 0xFE and 0xFC are pretty cool visuals for the last byte
-    fimul	dword [byte si+scaleconst]  ; fpu: const*cos(theta) theta, the constant is what ever the lines there assemble to
-    fidiv	word [bx-1]	                ; fpu: const*sin(theta)/x/256=1/r theta
-    fisub	word [time]                 ; fpu: 1/r+offset theta
-    fistp	dword [bx]                  ; store r+offset to where dx is, cx&dx affected after popa, fpu: theta
+    fimul   dword [byte si+scaleconst]  ; fpu: const*cos(theta) theta, the constant is what ever the lines there assemble to
+    fidiv   word [bx-1]                 ; fpu: const*sin(theta)/x/256=1/r theta
+    fisub   word [time]                 ; fpu: 1/r+offset theta
+    fistp   dword [bx]                  ; store r+offset to where dx is, cx&dx affected after popa, fpu: theta
     fnop                                ; this fnop will mutated to something more interesting eventually
     .effect2 equ $-1
-    fimul	word [time+3]	            ; fpu: t*theta (+3 is initially wrong, but will be replaced with time+0 i.e. correct)
+    fimul   word [time+3]               ; fpu: t*theta (+3 is initially wrong, but will be replaced with time+0 i.e. correct)
     .thetascale equ $-2
-    fistp	dword [bx+2]                ; store r+offset to where cx is, cx&ax affected after popa. We avoid messing the original IRQ address
-    popa				                ; pop all registers from stack
+    fistp   dword [bx+2]                ; store r+offset to where cx is, cx&ax affected after popa. We avoid messing the original IRQ address
+    popa                                ; pop all registers from stack
     mov     al, byte [byte si+envs+2]   ; we rotate the tunnel based on the last channel envelope
     add     ch, al
-    xor 	dh, ch		                ; dh = r, ch = theta
+    xor     dh, ch                      ; dh = r, ch = theta
     shl     dh, 1
     and     dh, 64                      ; we select parts of the XOR-texture
     add     al, byte [byte si+envs+1]   ; we add together the last two envelopes
@@ -47,14 +47,14 @@ main:                                   ; basic tunnel effect, based on HellMood
     .palette equ $-1
     stosb                               ; di = current pixel, write al to screen
     imul    di, 85                      ; traverse the pixels in slightly random order (tip from Hellmood)
-    mov 	ax, 0xCCCD		            ; Rrrola trick!
-    mul 	di                          ; dh = y, dl = x
+    mov     ax, 0xCCCD                  ; Rrrola trick!
+    mul     di                          ; dh = y, dl = x
     xchg    bx, ax                      ; HellMood: put the low word of multiplication to bx, so we have more precision
     jc      main                        ; when loading it in FPU
     xchg    ax, dx                      ; dx guaranteed zero
-    in 		al, 0x60                    ; check for ESC key
+    in      al, 0x60                    ; check for ESC key
     dec     ax
-    jnz	    main                        ; when song ends, this mutates to jnz -3 so it loops back to dec ax until ax = 0
+    jnz     main                        ; when song ends, this mutates to jnz -3 so it loops back to dec ax until ax = 0
     .looptarget equ $-1
     pop     dx
     pop     ds
@@ -63,7 +63,7 @@ main:                                   ; basic tunnel effect, based on HellMood
 setirq:
     out     40h, al                     ; write PIT counter divisor low byte
     salc                                ; set AL = 0 (because carry is zero)
-    out     40h, al	                    ; write PIT counter divisor high byte (freq = 1,19318181818 MHz / divisor)
+    out     40h, al                     ; write PIT counter divisor high byte (freq = 1,19318181818 MHz / divisor)
     xchg    al, bl                      ; set video mode
     int     10h
     mov     ax, 2508h
